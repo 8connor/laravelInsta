2886fd658c6e042958bca6db593f1bae41699f86
Connor Hooven <napster@Connors-MacBook-Pro.local>
2021-03-25 11:41:14 -0400

    adding in some stuff on the dashboard.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-3 p-5">
+                <img src="/images/defaultUser.png" alt="default profile pic" class="rounded-circle profilePic"
+                    height="100px" width="100px">
+            </div>
+            <div class="col-9">
+                <div></div>
+                <div>
+                    <div>
+                        
+                    </div>
+                    <div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
+                    <div>
+                        
+                    </div>
                 </div>
             </div>
+
         </div>
     </div>
-</div>
 @endsection
